c15c98363f6ba2946d7e867658160c3e969d6de4
joboyd4 <joboyd4@yahoo.com>
2018-04-05 12:01:41 -0400

    Added 2 echo comments for testing git

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
   <BODY>
     <?php
        echo "This is John's Test AWS App";
+       echo "This is a test of uploading to get via local Windows client";
+       echo "This is a test of installing on aws linux via git command line"
     ?>
   </BODY>
 </TITLE>
