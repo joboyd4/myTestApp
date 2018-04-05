fbfc1fc2723c665e9f09667fbf3a3d4d8129c84e
joboyd4 <joboyd4@yahoo.com>
2018-04-05 12:23:28 -0400

    Seperated php code into 2 blocks

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <HTML>
   <TITLE>John's Test AWS Web App</TITLE>
   <BODY>
+    <?php
+       function display()
+       {
+         echo "The what you wrote in the box was: ".$_POST["variable"]."<BR>";
+       }
+    ?>
     <FORM method="post" action="index.php">
       <INPUT type="text" name="variable">
       <INPUT type="submit" value="Submit" name="submit1">
@@ -10,10 +16,7 @@
        echo "This is a test of uploading to get via local Windows client<BR>";
        echo "This is a test of installing on aws linux via git command line<BR>";
        echo "This is a test of adding a php function<BR>";
-       function display()
-       {
-         echo "The what you wrote in the box was".$_POST["variable"];
-       }
+
        if(isset($_POST['submit1']))
        {
          display();
