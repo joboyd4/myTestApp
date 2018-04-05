1a4fa0b903677e35c0f7909ab81781e29299bb55
joboyd4 <joboyd4@yahoo.com>
2018-04-05 12:05:33 -0400

    Added /r/n for new lines

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
   <TITLE>John's Test AWS Web App</TITLE>
   <BODY>
     <?php
-       echo "This is John's Test AWS App";
-       echo "This is a test of uploading to get via local Windows client";
-       echo "This is a test of installing on aws linux via git command line"
+       echo "This is John's Test AWS App \r\n";
+       echo "This is a test of uploading to get via local Windows client \r\n";
+       echo "This is a test of installing on aws linux via git command line \r\n"
     ?>
   </BODY>
 </TITLE>
