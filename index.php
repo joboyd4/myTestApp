f3ad24f7d8aa319e34fef34fadae6eff48154854
joboyd4 <joboyd4@yahoo.com>
2018-04-05 12:08:28 -0400

    Changed /r/n to <BR> and added ;

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
   <TITLE>John's Test AWS Web App</TITLE>
   <BODY>
     <?php
-       echo "This is John's Test AWS App \r\n";
-       echo "This is a test of uploading to get via local Windows client \r\n";
-       echo "This is a test of installing on aws linux via git command line \r\n"
+       echo "This is John's Test AWS App<BR>";
+       echo "This is a test of uploading to get via local Windows client<BR>";
+       echo "This is a test of installing on aws linux via git command line<BR>";
     ?>
   </BODY>
 </TITLE>
