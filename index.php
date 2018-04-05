fd4f96eb145bd36af133396dd25cec6bf636b9d4
joboyd4 <joboyd4@yahoo.com>
2018-04-05 12:15:28 -0400

    Added Display function

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <HTML>
   <TITLE>John's Test AWS Web App</TITLE>
   <BODY>
+    <FORM method="post" action="display()">
+      <INPUT type="text" name="variable">
+      <INPUT type="submit" value="click">
+    </FORM>
     <?php
        echo "This is John's Test AWS App<BR>";
        echo "This is a test of uploading to get via local Windows client<BR>";
        echo "This is a test of installing on aws linux via git command line<BR>";
+       echo "This is a test of adding a php function<BR>";
+       function display()
+       {
+         echo "The what you wrote in the box was".$_POST["variable"];
+       }
     ?>
+
   </BODY>
 </TITLE>
