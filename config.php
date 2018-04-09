7307c793bc965cecb3e3a88ae536e7e143dc9d9b
joboyd4 <joboyd4@yahoo.com>
2018-04-09 13:38:09 -0400

    Testing Login for PHP

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost:3036');
+   //define('DB_USERNAME', 'root');
+   //define('DB_PASSWORD', 'rootpassword');
+   define('DB_DATABASE', 'MY_TEST_DATABASE');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
--- index.php
+++ index.php
@@ -7,21 +7,69 @@
          echo "The what you wrote in the box was: ".$_POST["variable"]."<BR>";
        }
     ?>
-    <FORM method="post" action="index.php">
-      <INPUT type="text" name="variable">
-      <INPUT type="submit" value="Submit" name="submit1">
-    </FORM>
     <?php
-       echo "This is John's Test AWS App<BR>";
-       echo "This is a test of uploading to get via local Windows client<BR>";
-       echo "This is a test of installing on aws linux via git command line<BR>";
-       echo "This is a test of adding a php function<BR>";
+       function login()
+       {
+         include("config.php");
+         //session_start();
 
-       if(isset($_POST['submit1']))
+         if(isset($_POST['loginTest']))
+         {
+           // username and password sent from form
+
+           $myusername = mysqli_real_escape_string($db,$_POST['username']);
+           $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+
+           $sql = "SELECT USER_ID FROM MY_USERS WHERE username = '$myusername' and passcode = '$mypassword'";
+           $result = mysqli_query($db,$sql);
+           $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+           $active = $row['active'];
+
+           $count = mysqli_num_rows($result);
+
+           // If result matched $myusername and $mypassword, table row must be 1 row
+           if($count == 1)
+           {
+             session_register("myusername");
+             $_SESSION['login_user'] = $myusername;
+
+             header("location: welcome.php");
+           }
+           else
+           {
+             $error = "Your Login Name or Password is invalid";
+           }
+         }
+       }
+    ?>
+
+    <?php
+       echo "<H1>This is a test of adding a php function to a form post</H1><BR>";
+
+       if(isset($_POST['formPostTest']))
        {
          display();
        }
     ?>
+
+    <FORM name='formPostTestForm' method="post" action="index.php">
+      <INPUT type="text" name="variable">
+      <INPUT type="submit" value="Submit" name="formPostTest">
+    </FORM>
+
+    <?php
+       echo "<H1>This is a test of adding a php function to a form post</H1><BR>";
+
+       if(isset($_POST['submit2']))
+       {
+         display();
+       }
+    ?>
+    <FORM name="loginTestForm" method="post" action="index.php">
+      <INPUT type="text" name="username">
+      <INPUT type="text" name="password">
+      <INPUT type="submit" value="Submit" name="loginTest">
+    </FORM>
 
   </BODY>
 </TITLE>
