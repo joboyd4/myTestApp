b357f83d37e708aa61664514e051360fa56c85c0
joboyd4 <joboyd4@yahoo.com>
2018-04-09 14:56:10 -0400

    test

--- config.php
+++ config.php
@@ -1,13 +1,12 @@
 <?php
-   define('DB_SERVER', 'localhost:3036');
+   define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'root123');
    define('DB_DATABASE', 'MY_TEST_DATABASE');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    /* check connection */
-   //IF (mysqli_connect_errno())
-   //{
-   //   printf("Connect failed: %s\n", mysqli_connect_error());
-  //    exit();
-   //}
+   IF (mysqli_connect_errno())
+   {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   }
 ?>
