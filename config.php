63b4709377f926e39bd539eb054b88abfb8abc85
joboyd4 <joboyd4@yahoo.com>
2018-04-17 11:17:29 -0400

    Remove PDO

--- config.php
+++ config.php
@@ -5,10 +5,10 @@
    //define('DB_PASSWORD', 'root123');
    //define('DB_DATABASE', 'MY_TEST_DATABASE');
    // Try and connect to the database
-  // $connection = mysqli_connect(DB_SERVER,$config['username'],$config['password'],$config['dbname']);
-   $connection = new PDO('mysql:dbname=$config['dbname'];host=localhost',$config['username'], $config['password']);
-   $connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-   $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   $connection = mysqli_connect(DB_SERVER,$config['username'],$config['password'],$config['dbname']);
+   //$connection = new PDO('mysql:dbname=$config['dbname'];host=localhost',$config['username'], $config['password']);
+   //$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+   //$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    // If connection was not successful, handle the error
    if($connection === false) {
       // Handle error - notify administrator, log to a file, show an error screen, etc.
