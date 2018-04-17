9f2a87b4f793534d89c7d28878ce9a73ed80a49a
joboyd4 <joboyd4@yahoo.com>
2018-04-17 11:22:06 -0400

    test

--- config.php
+++ config.php
@@ -6,9 +6,10 @@
    //define('DB_DATABASE', 'MY_TEST_DATABASE');
    // Try and connect to the database
    $connection = mysqli_connect(DB_SERVER,$config['username'],$config['password'],$config['dbname']);
-   //$connection = new PDO('mysql:dbname=$config['dbname'];host=localhost',$config['username'], $config['password']);
-   //$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-   //$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   $connection2 = new PDO('mysql:host=localhost;dbname=MY_TEST_DATABASE','root', 'root123');
+   echo $connection2;
+   $connection2->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+   $connection2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    // If connection was not successful, handle the error
    if($connection === false) {
       // Handle error - notify administrator, log to a file, show an error screen, etc.
